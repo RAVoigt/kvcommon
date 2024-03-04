f10dd1eac61570d7ac90d7ffc67914649184871c
Rob Voigt <code@ravoigt.com>
2024-03-04 04:11:30 +0100

    Lint fixes.

--- src/kvcommon/types.py
+++ src/kvcommon/types.py
@@ -8,18 +8,16 @@ def to_bool(val: None | bool | int | float | str) -> bool:
     if isinstance(val, int):
         if val == 0:
             return False
-        else:
-            return True
+        return True
     if isinstance(val, float):
         if val == 0.0:
             return False
-        else:
-            return True
+        return True
     if isinstance(val, str):
         val = val.strip().lower()
+        if val == "" or val in ["false", "no", "n", "0"]:
+            return False
         if val in ["true", "yes", "y", "1"]:
             return True
-        elif not val or val in ["false", "no", "n", "0"]:
-            return False
 
     raise ValueError(f"Unable to coerce value to boolean: {val}")
