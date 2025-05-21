0f06d9b8e2a8219bd2a8c4e72361a4e3d366db5a
Rob Voigt <code@ravoigt.com>
2025-05-21 17:38:10 +0200

    Remove all flask, prometheus and OTLP stuff - All moved to FlaskHammer.

--- src/kvcommon/flask_utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from kvcommon.exceptions import DependencyException
-
-try:
-    import flask
-
-except ImportError:
-    raise DependencyException("KVCommon: Must specify 'flask' extra to use flask features.")
