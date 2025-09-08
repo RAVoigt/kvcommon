67f8a006d3a478ae4bb6346e967148e58fefeef3
Rob Voigt <code@ravoigt.com>
2025-09-08 02:39:17 +0200

    K8s: Fix exporting of K8sAppsClient.

--- src/kvcommon/k8s/clients/__init__.py
+++ src/kvcommon/k8s/clients/__init__.py
@@ -1,9 +1,11 @@
+from .apps import K8sAppsClient
 from .core import K8sCoreClient
 from .custom_objects import K8sCustomObjectsClient
 from .networking import K8sNetworkingClient
 
 
 __all__ = [
+    "K8sAppsClient",
     "K8sCoreClient",
     "K8sCustomObjectsClient",
     "K8sNetworkingClient",
